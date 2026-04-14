f3bef16b5cba23d49b15640ec8390a2ac9a40771
codingweapon <prashantmistri348@gmail.com>
2026-04-14 16:08:46 +0530

    feat(ffmpeg-hls): integrate FFmpeg-based video processing with HLS segment generation in worker pool
    - implement FFmpeg processing in background workers
    - generate HLS playlist (.m3u8) and segment files (.ts)
    - stream processed video via HTTP endpoints
    - refactor worker pool logic to support video processing pipeline
    - update integration tests to cover end-to-end upload → store → processing → streaming flow

--- internal/worker/pool.go
+++ internal/worker/pool.go
@@ -1,14 +1,27 @@
 package worker
 
 import (
+	"context"
+	"fmt"
+	"io"
 	"log"
+	"media_processing_pipeline/config"
 	"media_processing_pipeline/internal/jobs"
-	"time"
+	"media_processing_pipeline/storage"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sync"
+
+	"github.com/minio/minio-go/v7"
 )
 
 type WorkerPool struct {
-	Queue chan jobs.Job
-	WorkerCount int
+	Queue         chan jobs.Job
+	WorkerCount   int
+	Env           *config.Env
+	StorageClient storage.ObjectStore
+	WaitGroup     *sync.WaitGroup
 }
 
 func (wp *WorkerPool) Start() {
@@ -18,18 +31,86 @@ func (wp *WorkerPool) Start() {
 }
 
 func (wp *WorkerPool) worker(id int) {
+	log.Printf("Worker %d started", id)
 	for job := range wp.Queue {
-		log.Printf("Worker %d started", id)
-		processJob(id, job)
+		wp.processJob(id, job, wp.Env)
+		wp.WaitGroup.Done()
 	}
 }
 
 func (wp *WorkerPool) Submit(job jobs.Job) {
+	wp.WaitGroup.Add(1)
 	wp.Queue <- job
 }
 
-func processJob(workerID int, job jobs.Job) {
+func (wp *WorkerPool) processJob(workerID int, job jobs.Job, env *config.Env) {
 	log.Printf("Worker %d picked job: %s\n", workerID, job.VideoID)
-	time.Sleep(5 * time.Second)
+
+	inputPath := fmt.Sprintf("tmp/%s.mp4", job.VideoID)
+
+	inputDir := "tmp"
+	outputDir := fmt.Sprintf("output/%s", job.VideoID)
+
+	os.MkdirAll(inputDir, os.ModePerm)
+	os.MkdirAll(outputDir, os.ModePerm)
+
+	obj, err := wp.StorageClient.GetObject(
+		context.Background(),
+		env.MinioBucketName,
+		job.FileKey,
+		minio.GetObjectOptions{},
+	)
+
+	if err != nil {
+		log.Println("Error getting object: ", err)
+		return
+	}
+	defer obj.Close()
+
+	// log.Printf("From processJob func: obj: %v", obj)
+	// log.Printf("size of obj: %v", unsafe.Sizeof(obj))
+	stat, err := obj.Stat()
+	if err != nil {
+		log.Println("Stat error:", err)
+	} else {
+		log.Println("Object size:", stat.Size)
+	}
+
+	file, err := os.Create(inputPath)
+	if err != nil {
+		log.Println("Error creating file: ", err)
+		return
+	}
+	defer file.Close()
+
+	// log.Printf("From processJob func: file: %v", file)
+
+	_, err = io.Copy(file, obj)
+	if err != nil {
+		log.Println("Error copying object: ", err)
+		return
+	}
+
+	cmd := exec.Command(
+		"ffmpeg",        // ffmpeg cmd
+		"-i", inputPath, // input file path
+		"-codec", "copy", // direct copy streams
+		"-start_number", "0", // segmentation numbers starts with 0
+		"-hls_time", "5", //
+		"-hls_list_size", "0", //
+		"-f", "hls", // output format
+		filepath.Join(outputDir, "index.m3u8"),
+	)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	log.Println("Starting FFmpeg...")
+
+	if err := cmd.Run(); err != nil {
+		log.Printf("FFmpeg failed for %s: %v\n", job.VideoID, err)
+		return
+	}
+
 	log.Printf("Worker %d finished processing: %s\n", workerID, job.VideoID)
 }
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"media_processing_pipeline/internal/worker"
 	"media_processing_pipeline/storage"
 	"net/http"
+	"sync"
 )
 
 func main() {
@@ -16,31 +17,46 @@ func main() {
 	if err := config.InitEnv(); err != nil {
 		log.Fatal(err)
 	}
-
+	// access env vars
 	env := config.LoadEnv()
 
+	// initialize minio clint
+	storage.InitMinIO(env)
+	// acccess the initialized minio client
+	client := storage.MinioClient
+	// initialize and access queue
 	queue := queue.InitQueue(100)
+	// create wait group
+	wg := &sync.WaitGroup{}
+	// create worker pool
 	pool := &worker.WorkerPool{
-		Queue:       queue,
-		WorkerCount: 3,
+		Queue:         queue,
+		WorkerCount:   3,
+		Env:           env,
+		StorageClient: client,
+		WaitGroup:     wg,
 	}
 
+	// start the worker pool
 	pool.Start()
 
+	// create handler
 	handler := &handlers.Handler{
-		Pool: pool,
+		Pool:          pool,
+		Env:           env,
+		StorageClient: client,
 	}
 
 	// initialize minio
-	storage.InitMinIO(env)
-	// create minio client
-	client := storage.MinioClient
+	fs := http.FileServer(http.Dir("output"))
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /", handlers.HomeHandler)
+	mux.HandleFunc("GET /video/", handlers.VideoHandler)
+	mux.Handle("GET /api/stream/", http.StripPrefix("/api/stream/", fs))
 	// pass minio client and bucket name as Dependency injection to upload handler
-	mux.HandleFunc("POST /api/upload", handler.UploadHandler(client, env.MinioBucketName))
+	mux.HandleFunc("POST /api/upload", handler.UploadHandler())
 
 	fmt.Printf("Server is running http://localhost:8080\n")
 	log.Fatal(http.ListenAndServe(":8080", mux))
